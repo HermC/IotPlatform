33d353c26b0fc3e214d9038bee615068e05646f4
Hermit <yzy627@126.com>
2019-03-27 13:43:46 +0800

    g

--- serial/tools/serial.js
+++ serial/tools/serial.js
@@ -1,15 +1,35 @@
 const SerialPort = require('serialport');
-const serialport = new SerialPort('COM4', {
+const portName = '/dev/tty.usbmodem141401';
+const serialport = new SerialPort(portName, {
     baudRate: 9600,
-    autoOpen: false
+    dataBits: 8,
+    parity: 'none',   //奇偶校验
+    stopBits: 1,   //停止位
+    // autoOpen: true,
+    flowControl: false
 });
 
-serialport.on('data', function(data) {
-    console.log('data received: ' + data);
+serialport.on('open', function(error) {
+    if (error) {
+        console.log('open serial port ' + portName + ' error');
+    } else {
+        console.log('open success');
+        let d = '';
+        serialport.on('data', function(data) {
+            let s = data.toString();
+            if (s && s.charAt(s.length - 1) === '\n') {
+                d += s;
+                console.log(parseFloat(d));
+                d = '';
+            } else {
+                d += s;
+            }
+        });
+    }
 });
 
 serialport.on('error', function(error) {
-
+    console.log('error: ' + error);
 });
 
 SerialPort.list(function(error, ports) {
